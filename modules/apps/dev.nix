eab880387554abd037f3eaddf893f0db78e1b5b9
Andy To <andya.to@gmail.com>
2026-07-25 00:33:52 -0400

    updated libvertd configs to allow for windows 11 TPM

--- modules/apps/dev.nix
+++ modules/apps/dev.nix
@@ -4,6 +4,8 @@ _: {
   flake.nixosModules.dev = { pkgs, config, ... }: {
     programs.virt-manager.enable = true;
     virtualisation.libvirtd.enable = true;
+    # Needed for guest vTPM (e.g. Windows 11's TPM 2.0 requirement).
+    virtualisation.libvirtd.qemu.swtpm.enable = true;
 
     # virt-manager needs the primary user in the libvirtd group.
     users.users.${config.hostConfig.user.name}.extraGroups = [ "libvirtd" ];
