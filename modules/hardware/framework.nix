e306607980a31cef2237084ae422c34b8e22c8d1
Andy To <andya.to@gmail.com>
2026-07-27 10:19:39 -0400

    fixed mic

--- modules/hardware/framework.nix
+++ modules/hardware/framework.nix
@@ -15,5 +15,14 @@ _: {
 
     services.upower.enable = true;
     networking.firewall.enable = true;
+
+    services.pipewire.wireplumber.extraConfig."51-framework-mic-fix" = {
+      "device.profile.priority.rules" = [
+        {
+          matches = [ { "device.name" = "alsa_card.pci-0000_c1_00.6"; } ];
+          actions.update-props.priorities = [ "HiFi (Mic1, Mic2, Speaker)" ];
+        }
+      ];
+    };
   };
 }
