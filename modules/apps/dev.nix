a645670aa508a0d2a880bfd04043ae25eb61a447
Andy To <andya.to@gmail.com>
2026-07-27 02:01:35 -0400

    added virtiofsd to allow for shared folders between host/guest

--- modules/apps/dev.nix
+++ modules/apps/dev.nix
@@ -6,6 +6,9 @@ _: {
     # Needed for guest vTPM (e.g. Windows 11's TPM 2.0 requirement).
     virtualisation.libvirtd.qemu.swtpm.enable = true;
 
+    # Needed for virtiofs shared folders between host and guest.
+    virtualisation.libvirtd.qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+
     users.users.${config.hostConfig.user.name}.extraGroups = [ "libvirtd" ];
 
     environment.systemPackages = with pkgs; [
