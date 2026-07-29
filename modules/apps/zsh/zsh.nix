9388a35dfbe38cd2ac309750b2e5c7cbe3294b1f
Andy To <andya.to@gmail.com>
2026-07-29 14:58:26 -0400

    added automatic load of a ~/.playground file from .zshrc so i can test things

--- modules/apps/zsh/zsh.nix
+++ modules/apps/zsh/zsh.nix
@@ -74,6 +74,7 @@ _: {
 
           source "${pkgs.asdf-vm}/etc/profile.d/asdf-prepare.sh"
           [ -f "$HOME/.alias" ] && source "$HOME/.alias"
+          [ -f "$HOME/.playground" ] && source "$HOME/.playground"
         ''
 
         # p10k's own convention is to source ~/.p10k.zsh last, after the theme
