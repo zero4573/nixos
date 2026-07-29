045e52021e0dd77c46ab5dd50a5edaad96959114
Andy To <andya.to@gmail.com>
2026-07-29 14:39:18 -0400

    added pkg-config

--- hosts/common.nix
+++ hosts/common.nix
@@ -88,6 +88,7 @@
       gnumake
       bind
       gcc
+      pkg-config
 
       # chattr/lsattr -- otherwise only pulled in as an internal dependency
       # of modules/home/nodatacow.nix, not on the interactive PATH.
