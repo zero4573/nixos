832c4fb6d7b2bd2105c3b368818a1343cf42bee0
Andy To <andya.to@gmail.com>
2026-08-14 10:09:35 -0400

    disable 3-finger tap (middleclick) to paste

--- modules/home/desktop-home.nix
+++ modules/home/desktop-home.nix
@@ -5,6 +5,13 @@
 
     home.stateVersion = osConfig.hostConfig.stateVersion;
 
+    # Disable GTK's middle-click/primary-selection paste. niri's touchpad
+    # tap-to-click maps a 3-finger tap to a middle-click, so
+    # a stray triple-tap pastes the primary selection into whatever's
+    # focused. There's no way to disable just that finger count, so this
+    # disables primary-selection paste globally instead.
+    dconf.settings."org/gnome/desktop/interface".gtk-enable-primary-paste = false;
+
     # Vicinae launcher daemon (autostarted via systemd user service).
     programs.vicinae = {
       enable = true;
